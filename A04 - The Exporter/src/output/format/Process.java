4ac599d50064f9be6dfd8765aa478cd9da3ae239
tgm-ttaschner <thomas.taschner@student.tgm.ac.at>
2015-01-18 14:00:29 +0100

    Finished Process

--- A04 - The Exporter/src/output/format/Process.java
+++ A04 - The Exporter/src/output/format/Process.java
@@ -1,22 +1,39 @@
 package output.format;
 
 import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class Process {
 
 	private ArrayList<String> formattedOutput;
-
-	private int line;
+	
+	private String seperator;
 
 	private ResultSet resultSet;
+	
+	private ResultSetMetaData rsmd;
 
-	public Process(ResultSet resultSet) {
+	public Process(ResultSet resultSet, String seperator) throws SQLException {
 		this.resultSet = resultSet;
+		this.seperator = seperator;
+		
+		rsmd = resultSet.getMetaData();
 	}
 
-	public String readLine() {
-		return null;
+	public ArrayList<String> readAll() throws SQLException {
+		while (resultSet.next())	{
+			
+			String column = "";
+			
+			for (int i = 0; i < rsmd.getColumnCount(); i++)	{
+				column += resultSet.getObject(i) + seperator + " ";
+			}
+			
+			formattedOutput.add(column);
+		}
+		
+		return formattedOutput;
 	}
-
 }
