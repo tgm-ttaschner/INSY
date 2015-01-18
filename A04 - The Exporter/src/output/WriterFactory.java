77a31f8c49d2238efe9c00f3e36838b65c2c6272
tgm-ttaschner <thomas.taschner@student.tgm.ac.at>
2015-01-18 17:02:51 +0100

    Finished WriterFactory

--- A04 - The Exporter/src/output/WriterFactory.java
+++ A04 - The Exporter/src/output/WriterFactory.java
@@ -1,9 +1,17 @@
 package output;
 
+import java.io.FileNotFoundException;
+
 public class WriterFactory {
 
-	public WriterFactory(String type) {
-		
+	public Writer chooseWriter(String type, String filename) throws FileNotFoundException {
+		switch (type) {
+		case "file":
+			return new FileWriter(filename);
+		case "console":
+			return new ConsoleWriter();
+		default:
+			return null;
+		}
 	}
-
 }
