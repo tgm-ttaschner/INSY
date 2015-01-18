5f7b2ad7f0761e6891fd4fab611cfd4d53e5f674
tgm-ttaschner <thomas.taschner@student.tgm.ac.at>
2015-01-18 18:27:12 +0100

    Fixed a small processing bug

--- A04 - The Exporter/src/output/format/Process.java
+++ A04 - The Exporter/src/output/format/Process.java
@@ -8,32 +8,38 @@ import java.util.ArrayList;
 public class Process {
 
 	private ArrayList<String> formattedOutput;
-	
+
 	private String seperator;
 
 	private ResultSet resultSet;
-	
+
 	private ResultSetMetaData rsmd;
 
 	public Process(ResultSet resultSet, String seperator) throws SQLException {
 		this.resultSet = resultSet;
 		this.seperator = seperator;
-		
+
+		formattedOutput = new ArrayList<String>();
+
 		rsmd = resultSet.getMetaData();
 	}
 
 	public ArrayList<String> readAll() throws SQLException {
 		while (resultSet.next())	{
-			
+
 			String column = "";
-			
-			for (int i = 0; i < rsmd.getColumnCount(); i++)	{
-				column += resultSet.getObject(i) + seperator + " ";
+
+			for (int i = 1; i <= rsmd.getColumnCount(); i++)	{
+				if (i == rsmd.getColumnCount())	{
+					column += resultSet.getObject(i);
+				} else {
+					column += resultSet.getObject(i) + seperator + " ";
+				}
 			}
-			
+
 			formattedOutput.add(column);
 		}
-		
+
 		return formattedOutput;
 	}
 }
